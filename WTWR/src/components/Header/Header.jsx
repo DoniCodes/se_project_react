6ca936a53de359a45fe1cf3799b9a38a82ef78f8
Adonis <adonis.santos02@icloud.com>
2025-03-01 16:59:53 -0500

    Make header show current date. Update header styles

--- WTWR/src/components/Header/Header.jsx
+++ WTWR/src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import "./Header.css";
+import { currentDate } from "../../utils/constants.js";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="WTWR Logo" />
-      <p className="header__date-location">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <p className="header__date-location">{currentDate}, LOCATION</p>
       <div className="header__user-container">
+        <button className="header__add-clothes-btn">+ Add Clothes</button>
         <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="" className="header__avatar" />
       </div>
+      <img src={avatar} alt="" className="header__avatar" />
     </header>
   );
 }
