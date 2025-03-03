117f1628abd6719aacb53b4f436189a36e56bbf3
Adonis <adonis.santos02@icloud.com>
2025-03-03 05:37:34 -0500

    Create ModalWithForm component. Add the form  markup as children in the ModalWithFormComponent in App jsx. Add css styles to ModalWithForm. Add functionality to opening and closing the form on button click

--- WTWR/src/components/App/App.jsx
+++ WTWR/src/components/App/App.jsx
@@ -4,15 +4,97 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleAddClick = () => {
+    setActiveModal("add garment");
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <div className="app">
       <div className="app__content">
-        <Header />
+        <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} />
         <Footer />
       </div>
+      <ModalWithForm
+        title="New Garment"
+        buttonText="Add Garment"
+        activeModal={activeModal}
+        handleCloseClick={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+            required
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image Url"
+            required
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="hot"
+              name="weather"
+              value="hot"
+              required
+              defaultChecked
+            />
+            <p className="modal__label modal__label_type_text">Hot</p>
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="warm"
+              name="weather"
+              value="warm"
+              required
+              defaultChecked
+            />
+            <p className="modal__label modal__label_type_text">Warm</p>
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input
+              type="radio"
+              className="modal__radio-input"
+              id="cold"
+              name="weather"
+              value="cold"
+              required
+            />
+            <p className="modal__label modal__label_type_text">Cold</p>
+          </label>
+        </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
--- WTWR/src/components/Header/Header.jsx
+++ WTWR/src/components/Header/Header.jsx
@@ -2,13 +2,19 @@ import "./Header.css";
 import { currentDate } from "../../utils/constants.js";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="WTWR Logo" />
       <p className="header__date-location">{currentDate}, LOCATION</p>
       <div className="header__user-container">
-        <button className="header__add-clothes-btn">+ Add Clothes</button>
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add Clothes
+        </button>
         <p className="header__username">Terrence Tegegne</p>
       </div>
       <img src={avatar} alt="" className="header__avatar" />
