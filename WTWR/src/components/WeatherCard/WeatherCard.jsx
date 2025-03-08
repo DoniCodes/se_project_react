532dc146c594c2a72451b40e39df84ec757018c7
Adonis <adonis.santos02@icloud.com>
2025-03-08 06:55:01 -0500

    Add logic for dynamic weather card feature

--- WTWR/src/components/WeatherCard/WeatherCard.jsx
+++ WTWR/src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,29 @@
 import "./WeatherCard.css";
-import sunny from "../../assets/sunny.png";
+import { weatherTypes, defaultWeather } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredType = weatherTypes.filter((type) => {
+    return (
+      type.condition === weatherData.condition && type.day === weatherData.isDay
+    );
+  });
+
+  let weatherType;
+
+  if (filteredType.length === 0) {
+    weatherType = defaultWeather[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherType = filteredType[0];
+  }
+
   return (
     <section className="weather-card">
       <p className="weather-card___temp">{weatherData.temp.F}&deg; F</p>
-      <img src={sunny} alt="" className="weather-card__image" />
+      <img
+        src={weatherType?.url}
+        alt={`Card showing ${weatherType?.day ? "day" : "night"} time`}
+        className="weather-card__image"
+      />
     </section>
   );
 }
