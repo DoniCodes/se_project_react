826313461be061b23ee5cca6724081b994ac57cd
Adonis <adonis.santos02@icloud.com>
2025-04-03 15:11:37 -0400

    Create toggle switch for temperature in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import "./Header.css";
 import { currentDate } from "../../utils/constants.js";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch.jsx";
 function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="WTWR Logo" />
       <p className="header__date-location">
         {currentDate}, {weatherData.city}
-      </p>
+      </p>{" "}
       <div className="header__user-container">
+        <ToggleSwitch />
         <button
           onClick={handleAddClick}
           type="button"
--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -2,10 +2,12 @@ import "./ToggleSwitch.css";
 
 function ToggleSwitch() {
   return (
-    <div className="toggle__switch">
-      <input type="checkbox" className="toggle__switch_checkbox" />
-      <span className="toggle__switch_slider"></span>
-    </div>
+    <label className="toggle-switch">
+      <input type="checkbox" className="toggle-switch__checkbox" />
+      <span className="toggle-switch__circle"></span>
+      <span className="toggle-switch__text toggle-switch__text-F">F</span>
+      <span className="toggle-switch__text toggle-switch__text-C">C</span>
+    </label>
   );
 }
 
