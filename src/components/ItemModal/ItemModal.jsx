dce3bd14725ddbbc11f78e6a39e459819412dcd1
Adonis <adonis.santos02@icloud.com>
2025-04-05 19:26:36 -0400

    Use imageUrl instead of link. Update onClick for delete button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,22 +1,22 @@
 import "./ItemModal.css";
 
-function ItemModal({ activeModal, card, onClose, onDeleteCard }) {
+function ItemModal({ isOpen, card, onClose, onDeleteCard }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+    <div className={`modal ${isOpen && "modal_opened"}`}>
       <div className="modal__content modal__content_type_image">
         <button
           onClick={onClose}
           type="button"
           className="modal__close modal__close_image"
         ></button>
-        <img src={card.link} alt={card.name} className="modal__image"></img>
+        <img src={card.imageUrl} alt={card.name} className="modal__image"></img>
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
         </div>
         <button
           onClick={() => {
-            onDeleteCard(card.name);
+            onDeleteCard(card);
           }}
           type="submit"
           className="modal__delete-btn"
