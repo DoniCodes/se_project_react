6d9ce737dd486cdb1da26d86a49fdce1736d1760
Adonis <adonis.santos02@icloud.com>
2025-04-05 19:24:58 -0400

    Destructure props. Add onClick handler for Add button and cards. Make ClothesSection render cards from server

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,18 +1,21 @@
 import "./ClothesSection.css";
 import ItemCard from "../ItemCard/ItemCard";
-import { defaultClothingItems } from "../../utils/constants";
 
-function ClothesSection({ onCardClick }) {
+function ClothesSection({ onCardClick, clothingItems, onAddClick }) {
   return (
     <div className="clothes-section">
       <div className="clothes-section__header">
         <p className="clothes-section__header_title">Your Items</p>
-        <button type="button" className="clothes-section__header_btn">
+        <button
+          onClick={onAddClick}
+          type="button"
+          className="clothes-section__header_btn"
+        >
           + Add Item
         </button>
       </div>
       <ul className="clothes-section__list">
-        {defaultClothingItems.map((item) => {
+        {clothingItems.map((item) => {
           return (
             <ItemCard key={item._id} item={item} onCardClick={onCardClick} />
           );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,14 +2,18 @@ import "./Profile.css";
 import Sidebar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 
-function Profile({ onCardClick }) {
+function Profile({ onCardClick, clothingItems, onAddClick }) {
   return (
     <div className="profile">
       <section className="profile__sidebar">
         <Sidebar />
       </section>
       <section className="profile__clothing-item">
-        <ClothesSection onCardClick={onCardClick} />
+        <ClothesSection
+          onCardClick={onCardClick}
+          clothingItems={clothingItems}
+          onAddClick={onAddClick}
+        />
       </section>
     </div>
   );
